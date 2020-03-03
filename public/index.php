df00576058a7d80bf4c44f444565381484263151
patrickLabes <patrick.labes@digistorm.com.au>
2020-03-03 15:08:36 +1000

    added checks

--- public/index.php
+++ public/index.php
@@ -1,17 +1,44 @@
-<?php 
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Goutte\Client;
+
 $client = new Client();
 
+$products = ['tv', 'dyson', 'printer', 'elvis'];
+$listItems = [];
+$foundItems = [];
+
 // Go to the symfony.com website
-$crawler = $client->request('GET', 'https://www.symfony.com/blog/');
+$crawler = $client->request('GET', 'https://www.gumtree.com.au/s-gold-coast/l3006035r50?ad=offering&price-type=free');
 
-$crawler->filter('h2 > a')->each(function ($node) {
-    print $node->text()."\n";
+// Creates an array of item titles
+$listItems[] = $crawler->filter('.user-ad-row__title')->each(function ($node, $i) {
+    // We are currently only grabbing the title I will need to grab 
+    // the whole add and then get the url and the title from there
+    $newitem = $node->text();
+    return $newitem;
 });
+$listItems = $listItems[0];
+
+// Loop though items and search for key items
+foreach ($listItems as $item) {
+    foreach ($products as $searchingFor) {
+        if (strpos(strtolower($item), $searchingFor) !== false) {
+            $foundItems[] = $item;
+        }
+    }
+}
 
 
-echo '1Hello World';
 
+header('Content-type: application/json');
+echo json_encode([
+    'result' => array_values($foundItems),
+    'error' => null
+]);
